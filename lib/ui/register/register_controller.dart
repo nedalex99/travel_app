9792452229bc18a6dc69fad819b5e84c7226196f
Viviana Sirbu <viviana.sirbu@itsmartsystems.eu>
2022-05-11 12:59:53 +0300

    Added dependencies

--- lib/ui/register/register_controller.dart
+++ lib/ui/register/register_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:travel_app/ui/login/login_screen.dart';
 import 'package:travel_app/utils/constants/validator.dart';
